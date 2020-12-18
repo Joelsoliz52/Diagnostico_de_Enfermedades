3ad0ce6b340477814ae9ce7564ccd15a3b7601de
JoelSoliz <Joel.Soliz@fundacion-jala.com>
2020-12-17 22:08:34 -0400

    Some changes in UI

--- app/src/main/java/com/wow/diseasediagnosisv2/ui/WelcomeActivity.java
+++ app/src/main/java/com/wow/diseasediagnosisv2/ui/WelcomeActivity.java
@@ -8,27 +8,21 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.wow.diseasediagnosisv2.R;
-import com.wow.diseasediagnosisv2.io.Diagnosis;
-import com.wow.diseasediagnosisv2.model.Question;
 
-public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class WelcomeActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        LinearLayout quiz = findViewById(R.id.next);
-        quiz.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.next:
-                Intent questions = new Intent(this, QuestionsActivity.class);
-                startActivity(questions);
-                break;
-        }
+        LinearLayout questions = findViewById(R.id.next);
+        questions.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent question = new Intent(getApplicationContext(), QuestionsActivity.class);
+                startActivity(question);
+            }
+        });
     }
 }
